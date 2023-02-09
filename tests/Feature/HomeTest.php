87ddd0f7d9dbf719ed1c9287129c23d0c4703fdb
NicolasRodriguezH <nicolasrhila@gmail.com>
2023-02-08 19:04:29 -0500

    commit initial

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testEmpty()
+    {
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertSee('No hay etiquetas');
+    }
+
+    public function testWithData()
+    {
+        $tag = Tag::factory()->create();
+
+        $this->assertNotEmpty($tag->name);
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertDontSee('No hay etiquetas')
+            ->assertSee($tag->name);
+    }
+}
