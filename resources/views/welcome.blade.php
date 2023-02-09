98fd893ac19ee9fe2a2ac74ba8933eaabe730790
NicolasRodriguezH <nicolasrhila@gmail.com>
2023-02-09 14:24:16 -0500

    Feat! styles of frontend added with TailwindCSS, APP finalized

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-    </head>
-    <body>
-        <h4>Listado de etiquetas</h4>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title>Laravel</title>
+</head>
+<body class="bg-gray-200 py-10">
+    <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+        <form action="tags" method="post" class="flex mb-4">
+            @csrf
+            <input type="text" name="name" id="" class="rounded-l bg-gray-200 p-4 w-full outline-none"
+                    placeholder="Nueva etiqueta">
+            <input type="submit" value="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none">
+        </form>
+        <h4 class="text-lg text-center mb-4">Listado de etiquetas</h4>
         <table>
-            @forelse ($tags as $tag)
+            @forelse( $tags as $tag )
                 <tr>
-                    <td> {{ $tag->name }} </td>
+                    <td class="border px-4 py-2">{{ $tag->name }}</td>
+                    <td class="px-4 py-2">
+                        <form action="tags/{{$tag->id}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Borrar" class="px-3 rounded bg-red-500 text-white">
+                        </form>
+                    </td>
                 </tr>
             @empty
                 <tr>
@@ -21,5 +36,6 @@
                 </tr>
             @endforelse
         </table>
-    </body>
+    </div>
+</body>
 </html>
