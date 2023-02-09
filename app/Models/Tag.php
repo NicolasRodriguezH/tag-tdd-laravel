782059b28179ff9ab7448bf2735791c6295b1397
NicolasRodriguezH <nicolasrhila@gmail.com>
2023-02-09 16:35:40 -0500

    Feat! I started with tests and added slug field in tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,4 +10,10 @@ class Tag extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function getSlugAttribute() {
+        return strtolower(
+            str_replace(' ', '-', $this->name)
+        );
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title>Laravel</title>
 </head>
-<body class="bg-gray-200 py-10">
+<body class="bg-black py-10">
     <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
         <form action="tags" method="post" class="flex mb-4">
             @csrf
@@ -20,6 +20,7 @@
             @forelse( $tags as $tag )
                 <tr>
                     <td class="border px-4 py-2">{{ $tag->name }}</td>
+                    <td class="border px-4 py-2">{{ $tag->slug }}</td>
                     <td class="px-4 py-2">
                         <form action="tags/{{$tag->id}}" method="post">
                             @csrf
